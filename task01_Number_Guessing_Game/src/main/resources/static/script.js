05f584dc9c01628bb31604116d405ec3fa2babf5
Himanshuagarwal21 <agarwalhimanshu81453@gmail.com>
2026-09-14 23:38:18 -0400

    Organize repository with numbergame folder

--- task01_Number_Guessing_Game/src/main/resources/static/script.js
+++ task01_Number_Guessing_Game/src/main/resources/static/script.js
@@ -1,7 +1,7 @@
 const API =
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1"
-        ? "http://localhost:8080"
+        ? "http://localhost:9090"
         : "https://codsoft-taskno1.onrender.com";
 let attempts = 0;
 
